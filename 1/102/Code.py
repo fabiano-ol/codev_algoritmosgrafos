e00dd1695bc23ee21a63573a7448a9ae625754c7
gramado <fabiano.oliveira@ime.uerj.br>
2023-12-28 13:17:40 -0300

    ex 1/102

---
+++ 1/102/Code.py
@@ -0,0 +1,30 @@
+.:75Hj1*YH1587:<H-6<:),)
+.:75Hj1*ZH1587:<Ho:).7t1;<)i,2THo:).7u)<:1Bi,2
+
+,-.Hw*<-:o:).7o:),-P8TH9Qb
+	KH+7,->
+	oHeHo:).7u)<:1Bi,2PQ
+	oVl-.161:vP8R9Q
+	.7:H1H16H:)6/-P9Qb
+		.7:H2H16H:)6/-PYT8SYQb
+			>HeH1R8S2
+			1.H2fYb
+				oVi,1+176):i:-;<)P>TH>UYQ
+			1.H1fXb
+				oVi,1+176):i:-;<)P>TH>U8Q
+	:-<=:6Ho
+	KH+7,->
+
+,-.Hm;+:->-:PoQb
+	8:16<PO#OTH-6,eOOQ
+	.7:H>H16HoV~PQb
+		8:16<P.O#C>EbCJTJV2716P5)8P4)5*,)H@bH;<:P@QTH;7:<-,PoVvP>QQQQE%OTH-6,eOOQ
+	8:16<PO%OQ
+
+?014-HP|:=-Qb
+	<:Ab
+		8HeH16<P-6<:),)PQQ
+		9HeH16<P-6<:),)PQQ
+	-@+-8<b
+		*:-)3
+	m;+:->-:Pw*<-:o:).7o:),-P8T9QQ
