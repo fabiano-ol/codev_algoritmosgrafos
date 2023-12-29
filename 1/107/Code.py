cc094ef653ffb1330985e11cb078acedd8f96214
gramado <fabiano.oliveira@ime.uerj.br>
2023-12-29 01:49:14 -0300

    ex 1/107

---
+++ 1/107/Code.py
@@ -0,0 +1,49 @@
+.:75Hj1*YH1587:<H-6<:),)
+.:75Hj1*ZH1587:<Ho:).7u)<:1Bi,2
+
+KH+7,->:-57>-
+,-.Hm0k419=-PoTH~Qb
+	.7:H1H16H:)6/-P4-6P~QQb
+		.7:H2H16H:)6/-P1SYTH4-6P~QQb
+			1.H67<HoVm0i:-;<)P~#1%T~#2%Qb
+				:-<=:6Hn)4;-
+	:-<=:6H|:=-
+KH+7,->:-57>-
+
+,-.Hk419=-;|)5\PoQb
+	KH+7,->
+	9HeHX
+	.7:H>YH16H:)6/-PYTHoV6SYQb
+		.7:H>ZH16H:)6/-P>YSYTHoV6SYQb
+			.7:H>[H16H:)6/-P>ZSYTHoV6SYQb
+				.7:H>\H16H:)6/-P>[SYTHoV6SYQb
+					1.Hm0k419=-PoTH#>YT>ZT>[T>\%Qb
+						9HSeHY
+	:-<=:6H9
+	KH+7,->
+
+,-.Ht-:o:).7PQb
+	oHeHo:).7u)<:1Bi,2PQ
+	6HeH16<P-6<:),)PQQ
+	1.H6HdHXb
+		6HeH^X
+		oVl-.161:vP6Q
+		.7:H=H16H:)6/-PYT6SYQb
+			.7:H>H16H:)6/-P=SYT6SYQb
+				oVi,1+176):i:-;<)P=TH>Q
+	-4;-b
+		5HeH16<P-6<:),)PQQ
+		oVl-.161:vP6Q
+		.7:H'H16H:)6/-P5Qb
+			=T>HeH-6<:),)PQV;841<PQ
+			=T>HeH16<P=QTH16<P>Q
+			oVi,1+176):i:-;<)P=TH>Q
+	:-<=:6Ho
+
+?014-HP|:=-Qb
+	<:Ab
+		oHeHt-:o:).7PQ
+	-@+-8<b
+		*:-)3
+	
+	8:16<Pk419=-;|)5\PoQQ
