2e3ee26c46a5f9ef1f48616e5c9357b0e851ab43
gramado <fabiano.oliveira@ime.uerj.br>
2024-01-10 09:41:23 -0300

    ex 4/102

---
+++ 4/102/Code.py
@@ -0,0 +1,95 @@
+.:75Hj1*YH1587:<H-6<:),)
+.:75Hj1*ZH1587:<Ho:).7t1;<)i,2
+.:75H+744-+<176;H1587:<H,-9=-
+
+KH+7,->:-57>-
+,-.H~1;1<),7PoTH>TH>)47:ev76-Qb
+	1.H>)47:eev76-b
+		:-<=:6HoVw*<-:i<:~P>Q#O~1;1<),7O%
+	-4;-b
+		oVw*<-:i<:~P>Q#O~1;1<),7O%HeH>)47:
+
+,-.Hj=;+)l7~-:<1+-PoTH:QbH
+	kHeH,-9=-PQHKH.14)
+	~1;1<),7PoTH:THXQ
+	.7:H:?H16HoVvP:THq<-:):{7*:-v7e|:=-Qb
+		kV)88-6,PP:T:?QQ
+	?014-H4-6PkQfXb
+		>T>?HeHkV8784-.<PQ
+		?HeH>?V~1B
+		1.H~1;1<),7PoTH?QHeeHUYb
+			~1;1<),7PoTH?TH>Q
+			.7:H?=H16HoVvP?THq<-:):{7*:-v7e|:=-Qb
+				=HeH?=V~1B
+				1.H~1;1<),7PoTH=QHeeHUYb
+					kV)88-6,PP?T?=QQ
+
+,-.Hl-<-:516)l1;<)6+1)m6<:-~-:<1+-;PoTH=TH>Qb
+	.7:H1H16HoV~PQb
+		oVl-.161:i<:~P1THCEQ
+		~1;1<),7PoTH1THUYQ
+	j=;+)l7~-:<1+-PoTH=Q
+	8HeH~1;1<),7PoTH>Q
+	kHeH#>TH8%
+	?014-H8HIeH=b
+		8HeH~1;1<),7PoTH8Q
+		kV)88-6,P8Q
+	kV:->-:;-PQ
+	:-<=:6Hk
+
+KH+7,->:-57>-
+
+,-.Hl-<-:516)l1;<)6+1)PoTH}THQb
+	KH+7,->
+	pHeHo:).7t1;<)i,2PQ
+	pVl-.161:vPoV6SZQ
+	.7:H>H16HoV~PQb
+		.7:H?H16HoVvP>Qb
+			1.H?HfH>b
+				pVi,1+176):i:-;<)P>TH?Q
+	.7:H>H16H}b
+		pVi,1+176):i:-;<)PoV6SYTH>Q
+	.7:H>H16Hb
+		pVi,1+176):i:-;<)PoV6SZTH>Q
+	=HeHoV6SYcH>HeHoV6SZ
+	
+	kHeHl-<-:516)l1;<)6+1)m6<:-~-:<1+-;PpTH=TH>Q
+	
+	:-<=:6Hk#Yb4-6PkQUY%
+	KH+7,->
+
+,-.Ht-:o:).7P6Qb
+	oHeHo:).7t1;<)i,2PQ
+	5HeH16<P-6<:),)PQQ
+	oVl-.161:vP6Q
+	.7:H'H16H:)6/-P5Qb
+		=T>HeH-6<:),)PQV;841<PQ
+		=T>HeH16<P=QTH16<P>Q
+		oVi,1+176):i:-;<)P=TH>Q
+	:-<=:6Ho
+
+?014-HP|:=-Qb
+	<:Ab
+		6HeH16<P-6<:),)PQQ
+	-@+-8<b
+		*:-)3
+	
+	1.H6HfeHXb
+		oHeHt-:o:).7P6Q
+		}HeH41;<P5)8P4)5*,)H@bH16<P@QTH-6<:),)PQV;841<PQQQ
+		HeH41;<P5)8P4)5*,)H@bH16<P@QTH-6<:),)PQV;841<PQQQ
+	-4;-b
+		6HeHZXXXY
+		oHeHo:).7t1;<)i,2PQ
+		}HeH#Y%cHHeH#YS6%
+		oVl-.161:vPZR6SYQ
+		oVi,1+176):i:-;<)PYTZR6SYQ
+		oVi,1+176):i:-;<)PYS6TZR6SYQ
+		.7:H1H16H:)6/-PZTH6THZQb
+			}V)88-6,P1QcH}V)88-6,P1S6Q
+			oVi,1+176):i:-;<)P1T1SYQ
+			oVi,1+176):i:-;<)P1SYTHZR6SYQ
+			oVi,1+176):i:-;<)P1S6T1S6SYQ
+			oVi,1+176):i:-;<)P1S6SYTHZR6SYQ
+	
+	8:16<Pl-<-:516)l1;<)6+1)PoT}TQQ
