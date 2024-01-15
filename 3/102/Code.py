10fbead2a3b5e04b46a37864ccc9a02d8f5f010a
gramado <fabiano.oliveira@ime.uerj.br>
2024-01-15 13:39:11 -0300

    ex 3/102

---
+++ 3/102/Code.py
@@ -0,0 +1,105 @@
+.:75Hj1*YH1587:<H-6<:),)
+.:75Hj1*ZH1587:<Ho:).7t1;<)i,2
+
+KH+7,->:-57>-
+,-.H~1;1<),7PoTH>TH>)47:ev76-Qb
+	1.H>)47:eev76-b
+		:-<=:6HoVw*<-:i<:~P>Q#O~1;1<),7O%
+	-4;-b
+		oVw*<-:i<:~P>Q#O~1;1<),7O%HeH>)47:
+
+,-.Hm0i:<PoTH>TH>)47:ev76-Qb
+	1.H>)47:eev76-b
+		:-<=:6HoVw*<-:i<:~P>Q#Om0i:<O%
+	-4;-b
+		oVw*<-:i<:~P>Q#Om0i:<O%HeH>)47:
+
+,-.Hm0l-5PoTH>TH>)47:ev76-Qb
+	1.H>)47:eev76-b
+		:-<=:6HoVw*<-:i<:~P>Q#Om0l-5O%
+	-4;-b
+		oVw*<-:i<:~P>Q#Om0l-5O%HeH>)47:
+
+,-.Hv1>-4PoTH>TH>)47:ev76-Qb
+	1.H>)47:eev76-b
+		:-<=:6HoVw*<-:i<:~P>Q#Ov1>-4O%
+	-4;-b
+		oVw*<-:i<:~P>Q#Ov1>-4O%HeH>)47:
+
+,-.Hx:7@z)1BPoTH>TH>)47:ev76-Qb
+	1.H>)47:eev76-b
+		:-<=:6HoVw*<-:i<:~P>Q#Ox:7@z)1BO%
+	-4;-b
+		oVw*<-:i<:~P>Q#Ox:7@z)1BO%HeH>)47:
+
+,-.Hj=;+)x:7.=6,1,),-PoTH>THx)1>Qb
+	~1;1<),7PoTH>TH|:=-Q
+	v1>-4PoTH>THYH1.Hx)1>eeXH-4;-Hv1>-4PoTHx)1>QSYQ
+	v:7n1407;HeHXcHn1407m0l-5HeHn)4;-
+	.7:H?H16HoVvP>Qb
+		1.H67<H~1;1<),7PoTH?Qb
+			j=;+)x:7.=6,1,),-PoTH?TH>Q
+			1.Hv1>-4PoTHx:7@z)1BPoTH?QQHdHv1>-4PoTHx:7@z)1BPoTH>QQb
+				x:7@z)1BPoTH>THx:7@z)1BPoTH?QQ
+			v:7n1407;HeHv:7n1407;HSHY
+			1.Hx:7@z)1BPoTH?QHeeH?H7:Hx:7@z)1BPoTH?QHeeH>b
+				m0l-5PoTH?TH|:=-Q
+				n1407m0l-5HeH|:=-
+		-4;-b
+			1.H?HIeHx)1>H)6,Hv1>-4PoTH?QHdHv1>-4PoTHx:7@z)1BPoTH>QQb
+				x:7@z)1BPoTH>TH?Q
+	1.Hx)1>HeeHXb
+		m0i:<PoTH>THv:7n1407;HfHYQ
+	-4;-b
+		m0i:<PoTH>THn1407m0l-5Q
+KH+7,->:-57>-
+
+,-.Hk76<):i:<j47+7;PoQb
+	KH+7,->
+	.7:H>H16HoV~PQb
+		oVl-.161:i<:~P>THCEQ
+		~1;1<),7PoTH>THn)4;-Q
+		m0i:<PoTH>THn)4;-Q
+		m0l-5PoTH>THn)4;-Q
+		v1>-4PoTH>THUYQ
+		x:7@z)1BPoTH>TH>Q
+	
+	j=;+)x:7.=6,1,),-PoTHYTHXQ
+
+	9)HeHXcH9*HeHX
+	.7:H>H16HoV~PQb
+		1.Hm0i:<PoTH>Qb
+			9)HSeHY
+		1.Hm0l-5PoTH>Qb
+			9*HSeHY
+	:-<=:6HP9)TH9*Q
+	KH+7,->
+
+,-.Ht-:o:).7P6Qb
+	oHeHo:).7t1;<)i,2PQ
+	5HeH16<P-6<:),)PQQ
+	oVl-.161:vP6Q
+	.7:H'H16H:)6/-P5Qb
+		=T>HeH-6<:),)PQV;841<PQ
+		=T>HeH16<P=QTH16<P>Q
+		oVi,1+176):i:-;<)P=TH>Q
+	:-<=:6Ho
+
+?014-HP|:=-Qb
+	<:Ab
+		6HeH16<P-6<:),)PQQ
+	-@+-8<b
+		*:-)3
+	
+	1.H6HfeHXb
+		oHeHt-:o:).7P6Q
+	-4;-b
+		6HeH\XX
+		oHeHo:).7t1;<)i,2PQ
+		oVl-.161:vP6R6SYQ
+		.7:H2H16H:)6/-PYTH6SYQb
+			oVi,1+176):i:-;<)PYTHP2UYQR6SZQ
+			.7:H1H16H:)6/-PYTH6Qb
+				oVi,1+176):i:-;<)PP2UYQR6S1SYTP2UYQR6S1SZQ
+		
+	8:16<Pk76<):i:<j47+7;PoQQ
