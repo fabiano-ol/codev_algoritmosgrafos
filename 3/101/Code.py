01c54a3ca8b860554bed071afcd31fb0d351c90e
gramado <fabiano.oliveira@ime.uerj.br>
2024-01-15 07:25:57 -0300

    ex 3/101

---
+++ 3/101/Code.py
@@ -0,0 +1,63 @@
+.:75Hj1*YH1587:<H-6<:),)
+.:75Hj1*ZH1587:<Ho:).7t1;<)i,2
+
+KH+7,->:-57>-
+,-.H~1;1<),7PoTH>TH>)47:ev76-Qb
+	1.H>)47:eev76-b
+		:-<=:6HoVw*<-:i<:~P>Q#O~1;1<),7O%
+	-4;-b
+		oVw*<-:i<:~P>Q#O~1;1<),7O%HeH>)47:
+
+,-.Hj=;+)x:7.=6,1,),-PoTH>QbH
+	~1;1<),7PoTH>TH|:=-Q
+	0HeHYcH0.YHeHXcH0.ZHeHXcH,eX
+	.7:H?H16HoVvP>Qb
+		1.H67<H~1;1<),7PoTH?Qb
+			P0.TH,.QHeHj=;+)x:7.=6,1,),-PoTH?Q
+			0HeH5)@P0TH0.SYQ
+			,HeH5)@P,TH,.Q
+			1.H0.f0.Zb
+				0.Ze0.
+			1.H0.Zf0.Yb
+				0.YT0.Ze0.ZT0.Y
+	,HeH5)@P,TH0.YS0.ZQ
+	:-<=:6HP0TH,Q
+KH+7,->:-57>-
+
+,-.Hl-<-:516)l1)5-<:7P|Qb
+	KH+7,->
+	.7:H>H16H|V~PQb
+		|Vl-.161:i<:~P>THCEQ
+		~1;1<),7P|TH>THn)4;-Q
+	P'TH,QHeHj=;+)x:7.=6,1,),-P|THYQ
+	:-<=:6H,
+	KH+7,->
+
+,-.Ht-:o:).7P6Qb
+	oHeHo:).7t1;<)i,2PQ
+	5HeH16<P-6<:),)PQQ
+	oVl-.161:vP6Q
+	.7:H'H16H:)6/-P5Qb
+		=T>HeH-6<:),)PQV;841<PQ
+		=T>HeH16<P=QTH16<P>Q
+		oVi,1+176):i:-;<)P=TH>Q
+	:-<=:6Ho
+
+?014-HP|:=-Qb
+	<:Ab
+		6HeH16<P-6<:),)PQQ
+	-@+-8<b
+		*:-)3
+	
+	1.H6HfeHXb
+		|HeHt-:o:).7P6Q
+	-4;-b
+		6HeH\XX
+		|HeHo:).7t1;<)i,2PQ
+		|Vl-.161:vP6R6SYQ
+		.7:H2H16H:)6/-PYTH6SYQb
+			|Vi,1+176):i:-;<)PYTHP2UYQR6SZQ
+			.7:H1H16H:)6/-PYTH6Qb
+				|Vi,1+176):i:-;<)PP2UYQR6S1SYTP2UYQR6S1SZQ
+		
+	8:16<Pl-<-:516)l1)5-<:7P|QQ
